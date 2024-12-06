d67a76d1a1caac108949999b0d9bb29cbde5519a
rikitoro <>
2024-12-06 20:27:36 +0900

    write README

---
+++ AbsoluteGeometry.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `AbsoluteGeometry` library.
+-- Import modules here that should be built as part of the library.
+import AbsoluteGeometry.Basic
---
+++ README.md
@@ -0,0 +1,7 @@
+# absolute-geometry
+
+安達[1]に基づいて、初等幾何の公理系のLeanによる形式化を試みる。
+
+1. 安達恒雄, よみがえる非ユークリッド幾何, 日本評論社 (2019)
+2. GeoCoq, https://geocoq.github.io/GeoCoq/
+
